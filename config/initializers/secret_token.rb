cbd744bdb45d90c446430abd8e419b6851db3ed3
ruemic <ruemic@me.com>
2011-12-18 19:07:28 -0800

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PipelineTool::Application.config.secret_token = '5a96f2d86b4f4a6999ddc2c07ed073dfbccb77d3cc9b7d670f34d5668b6cc53496bdd81be05779c207e2803bfc18b58b2de0dea91960c9d0a6654767b9dbdc93'
